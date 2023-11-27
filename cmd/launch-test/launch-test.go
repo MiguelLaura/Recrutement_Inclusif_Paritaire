c05299984a8fde691e6cb7ba5306d82979ecfdd8
Nathan <nathan.menny@etu.utc.fr>
2023-11-27 13:15:25 +0100

    Ajout d'un logger dans un fichier

--- cmd/launch-test/launch-test.go
+++ cmd/launch-test/launch-test.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"gitlab.utc.fr/mennynat/ia04-project/utils/logger"
 )
 
 func main() {
 
 	leLogger := logger.ConsoleLogger{}
+	leLoggerF, err := logger.NewFileLogger("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	loggers := logger.Loggers{}
 	loggers.AjouterLogger(&leLogger)
+	loggers.AjouterLogger(leLoggerF)
 
-	//loggers.Log("cc", "comment", "ça", "va", "?")
-
-	//leLogger.Log("cc", "comment", "ça", "va", "?")
+	loggers.Log("cc ", "comment ", "ça ", "va ", "?")
 }
