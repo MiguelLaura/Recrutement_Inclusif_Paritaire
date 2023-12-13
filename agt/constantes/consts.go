e8f7bea252200d814e4f9eed1652968ec6f7d7a7
SolennLenoir <solenn.lenoir@etu.utc.fr>
2023-12-13 17:21:39 +0100

    formule benefices + nv constantes

--- agt/constantes/consts.go
+++ agt/constantes/consts.go
@@ -12,7 +12,10 @@ const (
 	POURCENTAGE_RECRUTEMENT   = .05
 	NB_CANDIDATS_PAR_OFFRE    = 18 // Source : https://blog.flatchr.io/barometre-des-entreprises-qui-recrutent-deuxieme-semestre-2022
 	TIMEOUT_AGRESSION         = 4
-	POURCENTAGE_DEM_SPONTANEE = 0.027 // Taux de démission spontanée en France au 1er trimestre 2022
-	RETENTION_PARITE          = 0.2   // Au sein d'un organisation inclusive, les employés ont une rétention supérieure de 20% (source: Catalyst)
-	SEUIL_IMPACT_FEMME        = 0.35  // D'après Rosabeth Kanter, une minorité doit représenter au minimum 35% du groupe dominant pour avoir une influence
+	POURCENTAGE_DEM_SPONTANEE = 0.027  // Taux de démission spontanée en France au 1er trimestre 2022
+	RETENTION_PARITE          = 0.2    // Au sein d'un organisation inclusive, les employés ont une rétention supérieure de 20% (source: Catalyst)
+	SEUIL_IMPACT_FEMME        = 0.35   // D'après Rosabeth Kanter, une minorité doit représenter au minimum 35% du groupe dominant pour avoir une influence
+	CA_PAR_EMPLOYE            = 317000 // Source : https://xval.fr/chiffre-affaires-salarie/
+	COUT_EMPLOYE              = 50850  // Source : https://www.legisocial.fr/actualites-sociales/269-savez-vous-combien-coute-un-salarie-en-france.html
+	COUT_RECRUTEMENT          = 6500
 )
