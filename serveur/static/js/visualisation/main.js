c5f635438b0cf287eeb1da4e2605ceeb05bb022f
Nathan <nathan.menny@etu.utc.fr>
2023-12-17 16:15:34 +0100

    La partie logger de l'interface peut maintenant contenir autant de logs que l'on souhaite sans déborder

--- serveur/static/js/visualisation/main.js
+++ serveur/static/js/visualisation/main.js
@@ -3,7 +3,7 @@
 const exLogs = [
     "Employé #7 (H) a agressé Employé #0777 (F)",
     "Employé #0777 (F) a porté plainte contre Employé #7 (H)",
-    "Employé #76 (F) a posé sa démission"
+    "Employé #76 (F) a posé sa démission",
 ]
 
 for(let log of exLogs) {
