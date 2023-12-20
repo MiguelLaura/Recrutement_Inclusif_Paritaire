75040c7d50f2f1dadffe66e4bb2ad9b85ab9cc8b
Nathan <nathan.menny@etu.utc.fr>
2023-12-20 11:10:58 +0100

    Supprime les logs de démo

--- serveur/static/js/visualisation/main.js
+++ serveur/static/js/visualisation/main.js
@@ -1,14 +1,14 @@
 // Ajout des logs
 
-const exLogs = [
-    "Employé #7 (H) a agressé Employé #0777 (F)",
-    "Employé #0777 (F) a porté plainte contre Employé #7 (H)",
-    "Employé #76 (F) a posé sa démission",
-]
+// const exLogs = [
+//     "Employé #7 (H) a agressé Employé #0777 (F)",
+//     "Employé #0777 (F) a porté plainte contre Employé #7 (H)",
+//     "Employé #76 (F) a posé sa démission",
+// ]
 
-for(let log of exLogs) {
-    addLog(log);
-}
+// for(let log of exLogs) {
+//     addLog(log);
+// }
 
 // Initialise le graphe
 
