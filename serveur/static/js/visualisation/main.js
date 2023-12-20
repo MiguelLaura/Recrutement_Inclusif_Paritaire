a3e2b6963787dfcf9347407f72138468ce107260
Nathan <nathan.menny@etu.utc.fr>
2023-12-20 11:23:59 +0100

    suppression totale des démos

--- serveur/static/js/visualisation/main.js
+++ serveur/static/js/visualisation/main.js
@@ -1,15 +1,3 @@
-// Ajout des logs
-
-// const exLogs = [
-//     "Employé #7 (H) a agressé Employé #0777 (F)",
-//     "Employé #0777 (F) a porté plainte contre Employé #7 (H)",
-//     "Employé #76 (F) a posé sa démission",
-// ]
-
-// for(let log of exLogs) {
-//     addLog(log);
-// }
-
 // Initialise le graphe
 
 const leGraph = new Graph(document.getElementById('sim-graph'));
@@ -19,13 +7,6 @@ leGraph.addNewGraph("Bénéfices", [130, 174, 210]);
 leGraph.addNewGraph("Parité", [231, 54, 56]);
 
 leGraph.selectGraphs("Bénéfices", "Parité");
-
-// const dataBenef = [-2500, -1900, -10000, -4950, -4950, -3970, -5300];
-// const dataParite = [0, 600, 600, 1000, 500, 500, 1300];
-
-// for(let idxData = 0; idxData < dataBenef.length; idxData++) {
-//     leGraph.addData(dataBenef[idxData], dataParite[idxData]);
-// }
 
 btnGraphVisuTout.addEventListener("click", (evt) => {
     leGraph.selectGraphs("Bénéfices", "Parité");
