62ea646ac53ef227ba4fb3c70e5220c8aedde2f1
Nathan <nathan.menny@etu.utc.fr>
2024-01-05 15:06:24 +0100

    Ajout d'arguments pour lancer le programme

--- cmd/launch-all/launch-all.go
+++ cmd/launch-all/launch-all.go
@@ -1,13 +1,67 @@
+/*
+Crée le serveur web qui gère toutes les simulations.
+
+Utilisation :
+
+	launch-all [flags]
+
+Les flags peuvent être :
+
+	-h, --host nomHôte
+		Indique le nom de l'hôte.
+		Défaut : localhost
+
+	-p, --port numeroPort
+		Indique le port du serveur.
+		Défaut : 8080
+
+	-s
+		Lance le serveur en mode silencieux
+*/
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"log"
 
 	"gitlab.utc.fr/mennynat/ia04-project/serveur"
 )
 
 func main() {
-	server := serveur.NewRestServerAgent("localhost:8080")
+
+	// -------
+	// Traitement des flags
+
+	var host string
+	var port int
+	var silent bool
+
+	flag.StringVar(&host, "host", "localhost", "Hôte du serveur")
+	flag.StringVar(&host, "h", "localhost", "Hôte du serveur (raccourci)")
+
+	flag.IntVar(&port, "port", 8080, "Port du serveur")
+	flag.IntVar(&port, "p", 8080, "Port du serveur (raccourci)")
+
+	flag.BoolVar(&silent, "s", false, "Le serveur est en mode silencieux ou non")
+
+	flag.Parse()
+
+	if port < 0 {
+		log.Fatalf("Le numéro de port ne peut être négatif (donné %d)", port)
+	}
+
+	// ------
+	// Execution du script
+
+	if silent {
+		log.SetFlags(0)
+		log.SetOutput(io.Discard)
+	}
+
+	server := serveur.NewRestServerAgent(fmt.Sprintf("%s:%d", host, port))
 	server.Start()
+
 	fmt.Scanln()
 }
